92609a3126b1a056cf4fec3ef0f4fe9435146b07
cvharris <charris1014@gmail.com>
2016-02-26 13:52:20 -0600

    Revamps home and portfolio pages

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,17 +1,4 @@
 (function($) {
   $(document).ready(function() {
-    $('.page-content').click(function() {
-      $('.flipped').removeClass('flipped');
-    });
-    
-    $('.portfolio-project').click(function(e) {
-      e.stopPropagation();
-      if ($(this).hasClass('flipped')) {
-          $(this).removeClass('flipped');
-      } else {
-        $('.flipped').removeClass('flipped');
-        $(this).addClass('flipped');
-      }
-    })
   })
 })(jQuery);
